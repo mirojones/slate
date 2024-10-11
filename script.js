01992b6249fad0eccf17ed1f0e789ef2d4220e47
Miro <miropaxjones@gmail.com>
2024-10-12 10:55:04 +1100

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const canvas = document.getElementById("canvas")
+canvas.height = window.innerHeight
+canvas.width = window.innerWidth
+
+// ctx is the context of our canvas
+// we use ctx to draw on the canvas
+const ctx = canvas.getContext("2d")
+
+// lets create a rectangle for testing purposes
+ctx.fillStyle = "red"
+ctx.fillRect(100, 100, 100, 100)
